c24a58ed8ade42d62f09810f89582f89012d460c
DESKTOP-5VPIR8G\Dicky arya pratama <dickyarya.personal@gmail.com>
2021-04-20 13:44:51 +0800

    add animated splash screen

--- app/src/main/java/com/thorin/moviecotalogue/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/thorin/moviecotalogue/ui/splash/SplashActivity.kt
@@ -1,4 +1,34 @@
 package com.thorin.moviecotalogue.ui.splash
 
-class SplashActivity {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.bumptech.glide.Glide
+import com.thorin.moviecotalogue.R
+import com.thorin.moviecotalogue.databinding.ActivitySplashBinding
+import com.thorin.moviecotalogue.ui.home.HomeActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var splashActivityBinding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        splashActivityBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashActivityBinding.root)
+
+        Glide.with(this)
+            .load(R.drawable.ic_splash_screen)
+            .into(splashActivityBinding.imageSplash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val myIntent = Intent(this, HomeActivity::class.java)
+            startActivity(myIntent)
+            overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+            finish()
+        }, 3000)
+    }
+
 }
