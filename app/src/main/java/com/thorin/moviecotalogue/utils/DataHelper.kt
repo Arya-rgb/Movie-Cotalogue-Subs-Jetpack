7dc726ed867667baeee8c1f637b65b3c474e05ec
arya-rgb <dickyarya.personal@gmail.com>
2021-04-21 16:19:39 +0700

    fix image

--- app/src/main/java/com/thorin/moviecotalogue/utils/DataHelper.kt
+++ app/src/main/java/com/thorin/moviecotalogue/utils/DataHelper.kt
@@ -17,7 +17,7 @@ object DataHelper {
                 "Star Wars: The Last Jedi melanjutkan kisah Rey yang dikirim dalam misi untuk menemukan Luke di Acht-To, tempat kuil Jedi pertama kali didirikan. The Last Jedi juga akan membahas lebih lanjut hubungan antara Kylo Ren dan Rey. Rey meyakini kalau Kylo Ren masih memiliki sisi baik dalam dirinya, bahwa ia tak sepenuhnya gelap. Rey berusaha untuk menarik Kylo Ren ke sisi terang dan membelot dari Snoke. Bagi para fans Star Wars, tentu kamu wajib menonton The Last Jedi. Bagi kamu yang tidak mengikuti Star Wars dari awal, film ini tetap bisa dinikmati untuk penggemar film action.",
                 "Science Fiction",
                 "Amerika Serikat",
-                "https://i.ibb.co/Y2RN0wk/gwigwi-com-last-jedi-header.jpg"
+                "https://cdn.images.express.co.uk/img/dynamic/36/750x445/874321.jpg"
             )
         )
 
@@ -150,13 +150,8 @@ object DataHelper {
                 "T1",
                 "Game of Thrones",
                 "April 17, 2011",
-                "Rate Tinggi",
-                """
-                    Game of Throne bisa dibilang sebagai tv series terbaik dalam satu dekade terakhir. Hal itu dirasa tidak berlebihan, karena tv series garapan David Bennioff dan D.B. Weiss sukses merebut perhatian jutaan orang di seluruh dunia. Terbukti serial tv ini sukses mendapatkan rating tertinggi di situs IMDB, yaitu 9,5.
-                    Secara garis besar, tv series ini berkisah tentang perebutan tahta kekuasaan diantara para keluarga. Bahkan mereka sampai mempertaruhkan nyawa untuk mendapatkan tahta yang mereka idam-idamkan.
-                    Jalan cerita yang kompleks, imajinasi cerita liar, dan eksekusi apik membuat tv series yang dibintangi oleh Emilia Clarke, Kit Harrington, Sophie Turner, dan Lena Headey ini pantas menerima predikat tv series terbaik.
-                    Bulan April mendatang, tv series yang diangkat dari novel berjudul A Song of Ice and Fire ini akan mencapai di musim ke-8, yang merupakan musim terakhir dari Game of Thrones. Jadi jika kamu belum menyaksikan tv series ini, kamu bisa marathon Game of Thrones sampai sebelum berkahirnya perjalanan Jon Snow.
-                """.trimIndent(),
+                "Rating: 9.3/10 · \u200E1,796,962 votes",
+                "Game of Throne bisa dibilang sebagai tv series terbaik dalam satu dekade terakhir. Hal itu dirasa tidak berlebihan, karena tv series garapan David Bennioff dan D.B. Weiss sukses merebut perhatian jutaan orang di seluruh dunia. Terbukti serial tv ini sukses mendapatkan rating tertinggi di situs IMDB, yaitu 9,5.",
                 "Serial, Epic, Tragedy, Action, Fantasy television, Action fiction, Drama, Suspense, Medieval fantasy, Thriller, Adventure, Special Interest",
                 "Northern Ireland Dan Iceland",
                 "73 Episode",
